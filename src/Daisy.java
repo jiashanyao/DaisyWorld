a6b42b1c1c868c1c0eee2b4ad3bbde3cbdd6e9b5
Sheng LI <shengl6@student.unimelb.edu.au>
2019-05-23 05:31:01 +0000

    Upload New File

---
+++ src/Daisy.java
@@ -0,0 +1,29 @@
+/**
+ * The daisy in this world.
+ */
+public class Daisy {
+    //the age of daisy
+    private int age;
+    //the albedo of daisy
+    private double albedo;
+
+    //get the age of daisy
+    public int getAge() {
+        return age;
+    }
+
+    //set the age of daisy
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //get the albedo of daisy
+    public double getAlbedo() {
+        return albedo;
+    }
+
+    //set the albedo of daisy
+    public void setAlbedo(double albedo) {
+        this.albedo = albedo;
+    }
+}
