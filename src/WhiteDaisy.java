7ff0343b4f67fb38de318998bda07a1628f103b1
Sheng LI <shengl6@student.unimelb.edu.au>
2019-05-23 05:32:09 +0000

    Upload New File

---
+++ src/WhiteDaisy.java
@@ -0,0 +1,9 @@
+/**
+ * White daisies have a high albedo and thus reflect light
+ * and cool the area around them.
+ */
+public class WhiteDaisy extends Daisy{
+    public WhiteDaisy() {
+        super();
+    }
+}
