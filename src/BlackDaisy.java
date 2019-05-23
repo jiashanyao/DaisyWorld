dfc06320a6db214c79aba167830f6195cc9e56c3
Sheng LI <shengl6@student.unimelb.edu.au>
2019-05-23 05:30:32 +0000

    Upload New File

---
+++ src/BlackDaisy.java
@@ -0,0 +1,9 @@
+/**
+ * Black daisies have a low surface albedo and thus absorb light
+ * and heat the area around them.
+ */
+public class BlackDaisy extends Daisy {
+    public BlackDaisy(){
+        super();
+    }
+}
