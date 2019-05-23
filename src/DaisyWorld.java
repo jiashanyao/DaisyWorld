c202f98da9ade4bec6eb4e3517feaf4d3dfacd02
Sheng LI <shengl6@student.unimelb.edu.au>
2019-05-23 05:31:17 +0000

    Upload New File

---
+++ src/DaisyWorld.java
@@ -0,0 +1,35 @@
+/**
+ * DaisyWorld is a world filled with two different types of daisies:
+ * black daisies and white daisies. They differ in albedo, which is
+ * how much energy they absorb as heat from sunlight.
+ */
+public class DaisyWorld {
+    // start percentage of daisies
+    private double startBlacks;
+    private double startWhites;
+
+    // the albedo of daisies
+    private double albedoOfBlack;
+    private double albedoOfWhite;
+
+    private double solarLuminosity;
+
+    private String scenario;
+
+
+    // initialize the daisy world
+    public DaisyWorld(double startBlacks, double startWhites,
+            double albedoOfBlack, double albedoOfWhite,
+            double solarLuminosity, String scenario) {
+        this.startBlacks = startBlacks;
+        this.startWhites = startWhites;
+        this.albedoOfBlack = albedoOfBlack;
+        this.albedoOfWhite = albedoOfWhite;
+        this.solarLuminosity = solarLuminosity;
+        this.scenario = scenario;
+    }
+
+    public void run() {
+        while (true);
+    }
+}
