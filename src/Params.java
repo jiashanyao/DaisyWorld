8d90858fcd3bd1018055b5e6e01e16ace1af3e3d
Sheng LI <shengl6@student.unimelb.edu.au>
2019-05-23 05:31:30 +0000

    Upload New File

---
+++ src/Params.java
@@ -0,0 +1,22 @@
+public class Params {
+    // initial percentage of black daisies
+    public static double START_BLACK = 50;
+    // initial percentage of white daisies
+    public static double START_WHITE = 50;
+
+    public static double ALBEDO_OF_WHITE;
+    public static double ALBEDO_OF_BLACk;
+    public static double ALBEDO_Of_SURFACE;
+
+    // the max age of daisies
+    public static int MAX_AGE;
+
+    // the solar luminosity in different scenarios
+    public static Double RAMP = 0.8;
+    public static Double LOW = 0.6;
+    public static Double OUR = 1.0;
+    public static Double HIGH = 1.4;
+
+    // max ticks of the daisy world
+    public static int ticks;
+}
