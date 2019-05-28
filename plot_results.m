017d7b5d2622b7a56b480e85acffedb88e8a37a8
Jiashan Yao <yaojiashan95@gmail.com>
2019-05-28 23:07:38 +1000

    final submission

--- plot_results.m
+++ plot_results.m
@@ -7,11 +7,12 @@ white = data(4,:);
 soil = data(5,:);
 
 figure;
+subplot(2,1,1);
 plot(tick, temp);
 xlabel('Tick');
 ylabel('Global Temperature (\circC)');
 
-figure;
+subplot(2,1,2);
 plot(tick, black, tick, white, '--');
 xlabel('Tick');
 ylabel('Population');
